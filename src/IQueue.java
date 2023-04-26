07f4ddab8cb4d5eadb46d9cf01229414da0e2cbd
Barcino44 <juanjobargracia@gmail.com>
2023-04-26 13:01:40 -0500

    Adding the implementations of stacks and queues

---
+++ src/IQueue.java
@@ -0,0 +1,6 @@
+public interface IQueue<T> {
+    public boolean isEmpty();
+    public void enqueue(T value);
+    public T front();
+    public T dequeue();
+}
---
+++ src/IStack.java
@@ -0,0 +1,10 @@
+    public interface IStack <T> {
+
+        public boolean isEmpty();
+
+        public T top();
+
+        public void push(T obj);
+
+        public T pop();
+}
